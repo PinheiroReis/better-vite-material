18941306268f5c6132c9f30c75c736704f6bc31b
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-29 17:29:01 -0300

    home: bring back hover animatino on react and vite icons

--- src/pages/Home/components/Tools/index.tsx
+++ src/pages/Home/components/Tools/index.tsx
@@ -7,13 +7,39 @@ export default function IconsTools() {
     <Box sx={{ my: 4 }}>
       <Stack direction="row" spacing={4} justifyContent="center" alignItems="center">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} alt="Vite Logo" height={80} width={80} />
+          <Box
+            component="img"
+            src={viteLogo}
+            alt="Vite Logo"
+            height={80}
+            width={80}
+            sx={{
+              transition: 'all 0.3s ease',
+              '&:hover': {
+                transform: 'scale(1.2)',
+                filter: 'drop-shadow(0 0 10px #2196f3)',
+              },
+            }}
+          />
         </a>
         <Typography variant="h2" component="span">
           +
         </Typography>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} alt="React Logo" height={80} width={80} />
+          <Box
+            component="img"
+            src={reactLogo}
+            alt="React Logo"
+            height={80}
+            width={80}
+            sx={{
+              transition: 'all 0.3s ease',
+              '&:hover': {
+                transform: 'scale(1.2)',
+                filter: 'drop-shadow(0 0 10px #2196f3)',
+              },
+            }}
+          />
         </a>
       </Stack>
     </Box>
