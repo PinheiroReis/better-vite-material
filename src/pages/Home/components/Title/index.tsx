e2159bdadd43997a67c60198c12b8b4f4e4c63db
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-09-29 08:42:48 -0300

    all: install materia icons, font and fix title in home

--- src/pages/Home/components/Title/index.tsx
+++ src/pages/Home/components/Title/index.tsx
@@ -1,5 +1,9 @@
 import classes from './index.module.css';
 
 export default function Title() {
-  return <h1 className={classes.title}>The Better Vite Template</h1>;
+  return (
+    <h1 className={classes.title}>
+      <b>The Better Vite Template</b>
+    </h1>
+  );
 }
